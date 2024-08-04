ec34cb3b98b6e1200eeb2aaaec48c440abf40156
Vaishali Maheshwari <80218238+vaishalimaheshwari2000@users.noreply.github.com>
2024-08-04 16:32:05 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Tictactoe/tictac'
+
+function App() {
+  return (
+    <div className="App">
+   
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
